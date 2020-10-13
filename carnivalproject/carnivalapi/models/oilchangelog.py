10cee7daf61167a24c17226cdffc43e88c6e4b67
Tyler Simeone <tyler.simeone@gmail.com>
2020-10-13 18:50:28 -0500

    added vehicle model

--- carnivalproject/carnivalapi/models/oilchangelog.py
+++ carnivalproject/carnivalapi/models/oilchangelog.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 
 class OilChangeLog(models.Model):
     
-    vehicle = models.ForeignKey(Vehicle, related_name="vehicle", on_delete=models.CASCADE)
+    # vehicle = models.ForeignKey(Vehicle, related_name="vehicle", on_delete=models.CASCADE)
     oil_change_date = models.DateField(null=False, default="2000-01-01")
 
     class Meta:
