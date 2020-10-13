d7cb3993f4bc28266acec7e4f9614864eee495b3
Tyler Simeone <tyler.simeone@gmail.com>
2020-10-13 16:40:54 -0500

    added sale model

--- carnivalproject/carnivalapi/models/sale.py
+++ carnivalproject/carnivalapi/models/sale.py
@@ -1,20 +1,30 @@
 from django.db import models
 from django.urls import reverse
-from .user import User
+# from .saletype import SaleType
+# from .vehicle import Vehicle
 
-class Friend(models.Model):
+class Sale(models.Model):
     
-    sender = models.ForeignKey(User, related_name="sender", on_delete = models.CASCADE)
-    receiver = models.ForeignKey(User, related_name="receiver", on_delete = models.CASCADE)
-    requestPending = models.BooleanField(null=False, default=0)
-    requestAccepted = models.BooleanField(null=False, default=0)
+    # sales_type = models.ForeignKey(SaleType, related_name="saletype", on_delete = models.CASCADE)
+    # vehicle = models.ForeignKey(Vehicle, related_name="vehicle", on_delete = models.CASCADE)
+    # employee = models.ForeignKey(Employee, related_name="employee", on_delete = models.CASCADE)
+    # customer = models.ForeignKey(Customer, related_name="customer", on_delete = models.CASCADE)
+    # dealership = models.ForeignKey(Dealership, related_name="dealership", on_delete = models.CASCADE)
+    price = models.DecimalField(max_digits=8, decimal_places=2, null=False, default=1000)
+    deposit = models.IntegerField(null=False, default=1000)
+    purchase_date = models.DateField(null=False, default='2000-01-01')
+    pickup_date = models.DateField(null=False, default='2000-01-01')
+    invoice_number = models.CharField(null=False, max_length=50, default='1111111111')
+    payment_method = models.CharField(null=False, max_length=50, default='visa')
+    returned = models.BooleanField(null=False, default='false')
+
 
     class Meta:
-        verbose_name = ("Friend")
-        verbose_name_plural = ("Friends")        
+        verbose_name = ("Sale")
+        verbose_name_plural = ("Sales")        
         
     def __str__(self):
-        return f"Sender ID: {self.sender} Receiver ID: {self.receiver}"
+        return f"Sale ID: {self.pk}"
     
     def get_absolute_url(self):
-        return reverse("friend_detail", kwargs={"pk": self.pk})
+        return reverse("sale_detail", kwargs={"pk": self.pk})
