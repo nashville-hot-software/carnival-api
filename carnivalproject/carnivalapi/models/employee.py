762707c282568ed5dff6ab154c84b03713e44bb4
Your Name <you@example.com>
2020-10-13 16:57:45 -0500

    edited employees model

---
+++ carnivalproject/carnivalapi/models/employee.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
+
+class employee(models.Model):
+first_name = models.CharField(null=True, max_length=20)
+last_name = models.CharField(null=True, max_length=20)
+email_address = models.EmailField(max_length=254)
+phone = models.PhoneField(null=True, help_text='Contact phone number')
+employee_type_id = models.ForeignKey(employee, on_delete=models.CASCADE)
+isActive = models.CharField(null=True, max_length=20)
