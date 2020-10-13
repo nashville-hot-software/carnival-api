68482fc86947d00f890b5ce45d89c01466cc28c0
Your Name <you@example.com>
2020-10-13 10:44:52 -0500

    fixed dependies on pipfile

--- carnivalproject/carnivalproject/settings.py
+++ carnivalproject/carnivalproject/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'carnival_api',
         'USER': 'postgres',
-        'PASSWORD': '',
+        'PASSWORD': 'Kissed18!!',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
