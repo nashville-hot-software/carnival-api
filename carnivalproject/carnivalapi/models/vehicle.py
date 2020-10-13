6f3b26256b1d8c983abe2e3be6c70c913ddedcf6
Tyler Simeone <tyler.simeone@gmail.com>
2020-10-13 18:50:28 -0500

    added vehicle model

--- carnivalproject/carnivalapi/models/vehicle.py
+++ carnivalproject/carnivalapi/models/vehicle.py
@@ -1,20 +1,27 @@
 from django.db import models
 from django.urls import reverse
-from .user import User
+# from .vehicletype import VehicleType
 
-class Friend(models.Model):
+
+class Vehicle(models.Model):
     
-    sender = models.ForeignKey(User, related_name="sender", on_delete = models.CASCADE)
-    receiver = models.ForeignKey(User, related_name="receiver", on_delete = models.CASCADE)
-    requestPending = models.BooleanField(null=False, default=0)
-    requestAccepted = models.BooleanField(null=False, default=0)
+    vin = models.CharField(max_length=50, null=False, default="AAAAAAAAAAAAAAA")
+    engine_type = models.CharField(max_length=2, null=False, default="V6")
+    # vehicle_type = models.ForeignKey(VehicleType, related_name="vehicle type", on_delete=models.CASCADE)
+    exterior_color = models.CharField(max_length=50, null=False, default="Default Exterior Color")
+    interior_color = models.CharField(max_length=50, null=False, default="Default Interior Color")
+    floor_price = models.IntegerField(null=False, default=20000)
+    msr_price = models.IntegerField(null=False, default=15000)
+    miles_count = models.IntegerField(null=False, default=100)
+    year_of_car = models.IntegerField(null=False, default=2020)
+    is_sold = models.BooleanField(null=False, default=False)
 
     class Meta:
-        verbose_name = ("Friend")
-        verbose_name_plural = ("Friends")        
+        verbose_name = ("Vehicle")
+        verbose_name_plural = ("Vehicles")        
         
     def __str__(self):
-        return f"Sender ID: {self.sender} Receiver ID: {self.receiver}"
+        return f"Vehicle ID: {self.pk}"
     
     def get_absolute_url(self):
-        return reverse("friend_detail", kwargs={"pk": self.pk})
+        return reverse("vehicle_detail", kwargs={"pk": self.pk})
