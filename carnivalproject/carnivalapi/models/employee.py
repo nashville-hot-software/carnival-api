899c2b96c04ddc6d7b260e341f651f2bad6bf0c6
stephensenft21 <stephensenft21@gmail.com>
2020-10-14 13:25:51 -0500

    imported models into the __init__.py file

--- carnivalproject/carnivalapi/models/employee.py
+++ carnivalproject/carnivalapi/models/employee.py
@@ -5,8 +5,9 @@ from django.contrib.auth.models import User
 
 class employee(models.Model):
 
+    first_name = models.CharField(null=True, max_length=20)
 
-first_name = models.CharField(null=True, max_length=20)
+
 last_name = models.CharField(null=True, max_length=20)
 email_address = models.EmailField(max_length=254)
 phone = models.PhoneField(null=True, help_text='Contact phone number')
