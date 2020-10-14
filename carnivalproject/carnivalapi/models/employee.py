b29e23496e0d7f70e58d6b3f91b78a5909d5a159
Your Name <you@example.com>
2020-10-14 00:36:00 -0500

    adding employee init

--- carnivalproject/carnivalapi/models/employee.py
+++ carnivalproject/carnivalapi/models/employee.py
@@ -2,7 +2,10 @@ from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
 
+
 class employee(models.Model):
+
+
 first_name = models.CharField(null=True, max_length=20)
 last_name = models.CharField(null=True, max_length=20)
 email_address = models.EmailField(max_length=254)
