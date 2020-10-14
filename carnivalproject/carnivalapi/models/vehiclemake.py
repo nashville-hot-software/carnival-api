d1e2e26fa84a7bf4ee877a1fcfbe50625c266614
stephensenft21 <stephensenft21@gmail.com>
2020-10-14 14:10:32 -0500

    vehiclebodytype and vehiclemake models complete

--- carnivalproject/carnivalapi/models/vehiclemake.py
+++ carnivalproject/carnivalapi/models/vehiclemake.py
@@ -1,6 +1,18 @@
+from django.db import models
+from django.urls import reverse
 
 
+class VehicleMake(models.Model):
+
+    name = models.CharField(_null=True, max_length=50)
+
+    class Meta:
+        verbose_name = ("Vehicle Make")
+        verbose_name_plural = ("Vehicle Makes")
 
 
-class vehiclemake(models.Model): 
+    def __str__(self):
+        return f"VehicleMake ID: {self.pk}"
     
+    def get_absolute_url(self):
+        return reverse("vehicle_make_detail", kwargs={"pk": self.pk})
