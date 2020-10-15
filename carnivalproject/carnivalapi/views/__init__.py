17c3759e16b4e1e79628c154aae48aab98ccc651
Tyler Simeone <tyler.simeone@gmail.com>
2020-10-15 17:54:48 -0500

    added viewset for dealershipemployees, but cant test yet due to FK not existing yet

--- carnivalproject/carnivalapi/views/__init__.py
+++ carnivalproject/carnivalapi/views/__init__.py
@@ -3,3 +3,4 @@ from .dealership import Dealerships
 from .sale import Sales
 from .account_receivable import AccountsReceivable
 from .carrepairtype import CarRepairTypes
+from .dealershipemployee import DealershipEmployees
