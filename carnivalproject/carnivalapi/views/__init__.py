bc8f4deabc977426d4048f9a5faed050faa89639
Tyler Simeone <tyler.simeone@gmail.com>
2020-10-15 17:39:28 -0500

    added car repair viewset to url patterns

--- carnivalproject/carnivalapi/views/__init__.py
+++ carnivalproject/carnivalapi/views/__init__.py
@@ -2,3 +2,4 @@ from .customer import Customers
 from .dealership import Dealerships
 from .sale import Sales
 from .account_receivable import AccountsReceivable
+from .carrepairtype import CarRepairTypes
