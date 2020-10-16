09b98eb5570cd3661ca069770e75ceabb04bcfe5
Julian Garcia <garcia.j.julian@gmail.com>
2020-10-16 11:00:03 -0500

    added last 4 views

---
+++ carnivalproject/carnivalapi/views/repairtype.py
@@ -0,0 +1,89 @@
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers
+from rest_framework import status
+from ..models import RepairType
+
+class RepairTypeSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = RepairType
+        url = serializers.HyperlinkedIdentityField(
+            view_name='RepairType',
+            lookup_field='id'
+        )
+        fields = ('id', 'name')
+
+class RepairType(ViewSet):
+
+    def create(self, request):
+    
+        new_repairtype = RepairType()
+
+        new_repairtype.name = request.data["name"]
+
+        # FOR POSTMAN TESTING
+        # {
+        #     "name": "nameTesting"
+        # }
+
+
+        new_repairtype.save()
+
+        serializer = RepairTypeSerializer(
+            new_repairtype, context={'request': request})
+
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+
+        try:
+            repairtype = RepairType.objects.get(pk=pk)
+
+            serializer = RepairTypeSerializer(
+                repairtype, context={'request': request})
+
+            return Response(serializer.data)
+
+        except Exception as ex:
+            return HttpResponseServerError(ex)
+
+    def put(self, request, pk=None):
+        """Handle PUT requests for an individual repair type
+        Returns:
+            Response -- Empty body with 204 status code
+        """
+        repairtype = RepairType.objects.get(pk=pk)
+
+        new_repairtype.name = request.data["name"]
+
+        repairtype.save()
+
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+    def destroy(self, request, pk=None):
+        """Handle DELETE requests for a single repair type
+        Returns:
+            Response -- 200, 404, or 500 status code
+        """
+        try:
+            repairtype = RepairType.objects.get(pk=pk)
+            repairtype.delete()
+
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+        except repairtype.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    def list(self, request):
+        
+        repairtype = RepairType.objects.all()
+
+        serializer = RepairTypeSerializer(
+            repairtype, many=True, context={'request': request})
+
+        return Response(serializer.data)
---
+++ carnivalproject/carnivalapi/views/vehicletype.py
@@ -0,0 +1,95 @@
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers
+from rest_framework import status
+from ..models import VehicleType
+
+class VehicleTypeSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = VehicleType
+        url = serializers.HyperlinkedIdentityField(
+            view_name='VehicleType',
+            lookup_field='id'
+        )
+        fields = ('id', 'body_type_id', 'make_id', 'model_id')
+
+class VehicleType(ViewSet):
+
+    def create(self, request):
+    
+        new_vehicletype = VehicleType()
+
+        new_vehicletype.body_type_id = request.data["bodyTypeId"]
+        new_vehicletype.make_id = request.data["makeId"]
+        new_vehicletype.model_id = request.data["modelId"]
+
+        # FOR POSTMAN TESTING
+        # {
+        #     "body_type_id": "BodyTypeId"
+        #     "make_id": "MakeId"
+        #     "model_id": "ModelId"
+        # }
+
+
+        new_vehicletype.save()
+
+        serializer = VehicleTypeSerializer(
+            new_vehicletype, context={'request': request})
+
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+
+        try:
+            vehicletype = VehicleType.objects.get(pk=pk)
+
+            serializer = VehicleTypeSerializer(
+                vehicletype, context={'request': request})
+
+            return Response(serializer.data)
+
+        except Exception as ex:
+            return HttpResponseServerError(ex)
+
+    def put(self, request, pk=None):
+        """Handle PUT requests for an individual vehicle type
+        Returns:
+            Response -- Empty body with 204 status code
+        """
+        vehicletype = VehicleType.objects.get(pk=pk)
+
+        new_vehicletype.body_type_id = request.data["bodyTypeId"]
+        new_vehicletype.make_id = request.data["makeId"]
+        new_vehicletype.model_id = request.data["modelId"]
+
+        vehicletype.save()
+
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+    def destroy(self, request, pk=None):
+        """Handle DELETE requests for a single vehicletype
+        Returns:
+            Response -- 200, 404, or 500 status code
+        """
+        try:
+            vehicletype = VehicleType.objects.get(pk=pk)
+            vehicletype.delete()
+
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+        except vehicletype.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    def list(self, request):
+        
+        vehicletype = VehicleType.objects.all()
+
+        serializer = VehicleTypeSerializer(
+            vehicletype, many=True, context={'request': request})
+
+        return Response(serializer.data)
